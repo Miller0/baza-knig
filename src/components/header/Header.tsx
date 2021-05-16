c81b028a325788d6e6a279795ee7c34df3a65df9
Miller <mr.miller2017.11@gmail.com>
2021-05-16 17:25:54 +0300

    init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="row">
+                <div className="logo">
+                <a href="/" className="logo-img"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
